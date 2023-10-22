9664ec5a27c1d1eb0fe28509e6c5f145c4a0bfb9
Ruben Arcos <ruben.arcos06@gmail.com>
2023-10-22 00:27:24 -0500

    create login function route.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,23 @@
 const db = require("../utils/db");
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 
 const allUsers = (req, res) => {
-    const sql = "SELECT * FROM users"
-    db.query(sql, (error, rows) => {
-        res.json({ rows })
-    })
-}
+  const sql = "SELECT * FROM users";
+  db.query(sql, (error, rows) => {
+    res.json({ rows });
+  });
+};
 
 const userById = (req, res) => {
-    const sql = "SELECT * FROM users WHERE user_id = ?"
-    const { userId } = req.params
-    const body = [userId]
+  const sql = "SELECT * FROM users WHERE user_id = ?";
+  const { userId } = req.params;
+  const body = [userId];
 
-    db.query(sql, body, (error, rows) => {
-        res.json({ rows })
-    })
-}
+  db.query(sql, body, (error, rows) => {
+    res.json({ rows });
+  });
+};
 
 const createUser = async (req, res) => {
   console.log("inside /create-user route");
@@ -60,4 +61,59 @@ const createUser = async (req, res) => {
   });
 };
 
-module.exports = { createUser, allUsers, userById };
+const login = (req, res) => {
+  console.log("inside login route");
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(req.body);
+
+  const sql = `
+    SELECT * FROM users 
+    WHERE email = ?;
+  `;
+  // no password inside query, since it won't match the hash password
+  // argon2 will validate password and hash  password match
+  const body = [email];
+  db.query(sql, body, async (error, rows) => {
+    if (error) {
+      return res.json({ error });
+    }
+    // handling if user exist with email
+    if (rows.length === 0) {
+      return res.json({
+        msg: "Username does not exist. Please create account.",
+      });
+    }
+    const hash = rows[0].password;
+    let match;
+    try {
+      match = await argon2.verify(hash, password);
+    } catch (error) {
+      console.log(error);
+    }
+    // handling if password doesn't match
+    console.log(match, "match");
+    if (!match) {
+      // keep msg ambiguous
+      return res.json({
+        msg: "Username or password is incorrect. Please try again",
+      });
+    }
+    // user exists with matching password
+    // user authorized, sending auth token using jwt
+    // creating object with key/values
+    console.log(rows[0]);
+    const unsignedToken = {
+        id: rows[0].user_id,
+      email: rows[0].email,
+      // value coming from mysql database
+      firstName: rows[0].first_name,
+      lastName: rows[0].last_name,
+    };
+    //
+    const token = jwt.sign(unsignedToken, process.env.JWT_SECRET);
+    return res.json({ msg: "login successful", token });
+  });
+};
+
+module.exports = { createUser, allUsers, userById, login };
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { createUser, allUsers, userById } = require("../controllers/authController");
+const { createUser, allUsers, userById, login} = require("../controllers/authController");
 
 router.get("/", allUsers)
 router.get("/:userId", userById)
 router.post("/create-user", createUser);
+router.post("/login", login)
 
 module.exports = router;
