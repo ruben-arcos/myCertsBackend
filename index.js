79abd5a359f3461a1974308b65cfb74825fc4028
Ruben Arcos <ruben.arcos06@gmail.com>
2023-10-22 00:54:28 -0500

    added cors package, then added it to index.js to allow cross origin resource sharing

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const authRouter = require("./routers/authRouter")
+const cors = require("cors");
+const authRouter = require("./routers/authRouter");
 
 const app = express();
 const port = process.env.PORT || 3330;
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/users', authRouter)
+app.use("/users", authRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome to my server");
